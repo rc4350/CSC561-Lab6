b559f7d0c40fc9d2e4c8f53bc8d46d08e91ae45c
Unknown <deemarashdan@gmail.com>
2017-06-25 19:31:18 -0400

    fixed some methodes

--- src/environment/Environment.java
+++ src/environment/Environment.java
@@ -317,8 +317,8 @@ public class Environment
 	public Weapon getWeapon( int row, int col)
 	{
 			return cell[row][col].getWeaponOne();
+	}
 
-<<<<<<< Updated upstream
 	public LifeForm findNearest(LifeForm focus)
 	{
 		LifeForm nearest = null;
@@ -458,8 +458,7 @@ public class Environment
 		else
 			return null;
 		return null;
-=======
->>>>>>> Stashed changes
+
 	}
 	/**
 	 * the attacker life attacks the nearest enemy if able
